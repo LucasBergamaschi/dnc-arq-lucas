953be19095c7020c4685d306598639647fc37ecb
unknown <lucasmelomtb@gmail.com>
2025-03-04 11:00:52 -0300

    Atualizando projeto com correções e build para deploy na Vercel

--- dnc-arq-lucas/vite.config.js
+++ dnc-arq-lucas/vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  root: ".", // Define a raiz corretamente
   plugins: [react()],
+  build: {
+    outDir: "dist", // Define a pasta onde será gerado o build
+  }
 })
